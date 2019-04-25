0e74a246b539ab286254916b0866486e12c27a26
Gabriel <gabriel-silva_2011@hotmail.com>
2019-04-25 16:00:42 -0300

    finalizando navegação

--- src/components/Pages/ProductDetailPage/index.js
+++ src/components/Pages/ProductDetailPage/index.js
@@ -1,10 +1,17 @@
 import {createStackNavigator} from "react-navigation";
 
 import ProductDetailPage from "./ProductDetailPage";
+import RegisterVerifyCpfCnpjPage from "../RegisterVerifyCpfCnpjPage";
 
 export default createStackNavigator(
     {
         ProductDetailPage: {screen: ProductDetailPage},
+        RegisterVerifyCpfCnpjPage: {
+            screen: RegisterVerifyCpfCnpjPage,
+            navigationOptions: {
+                header: null
+            }
+        },
         initialRouteName: "ProductDetailPage",
     },
     {
